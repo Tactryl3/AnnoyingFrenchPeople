2b73f1aceed9b589152b3c7f8c2de0c343d1e799
Nic Evans <nicthomasnnt@gmail.com>
2020-12-23 21:13:59 -0500

    removed line

--- AnnoyingFrenchPeople.plugin.js
+++ AnnoyingFrenchPeople.plugin.js
@@ -8,7 +8,6 @@ var AnnoyingFrenchPeople = (function () {
         start() {
             var croissant = new Audio('https://nevana.dev/french.mp3');
             this.playCroissant = () => croissant.play()
-            
             document.addEventListener("click", this.playCroissant)
         }
         stop() { document.removeEventListener("click", this.playCroissant) }
